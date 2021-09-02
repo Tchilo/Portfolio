72ba26d5e854970320f279af301a362ec1c96432
Tchilo <tchiloross@gmail.com>
2021-09-02 10:45:31 +0200

    Fix height of modal popup

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const openMenuButton = document.querySelector('.open-menu-btn');
 const closeMenuButton = document.querySelector('.fa-times');
 const mobileMenu = document.querySelector('.d-menu-li');
 const links = document.querySelectorAll('.nav-bar-list li a');
-const popupData = document.querySelector('.popup-modal');
 function grab(e) {
   return document.getElementById(e);
 }
@@ -23,132 +22,122 @@ window.addEventListener('resize', () => {
   }
 });
 
-// Popup data array
-
-const mobilePopup = [
-  {
-    image: 'img/modal-popup.png',
-    title: 'Keeping track of hundreds of components',
-    languages: ['Ruby on rails', 'Css', 'JavaScript'],
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-             took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy 
-            text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-             text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-    links: ['See Live', 'See Source'],
-  },
-];
-
 // Project cards data
 
 const projectCards = [
   {
     id: 'card1',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['https://www.google.com', 'https://github.com/'],
   },
   {
     id: 'card2',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['https://www.google.com', 'https://github.com/'],
   },
   {
     id: 'card3',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['https://www.google.com', 'https://github.com/'],
   },
   {
     id: 'card4',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['www.google.com', 'www.facebook.com'],
   },
   {
     id: 'card5',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['https://www.google.com', 'https://github.com/'],
   },
   {
     id: 'card6',
-    image: '',
+    image: 'img/modal-popup.png',
     name: 'Multi-Post Stories Gain+Glory',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     button: 'See Project',
+    links: ['https://www.google.com', 'https://github.com/'],
   },
 ];
 
-// popup data template
+function fooBar(position) {
+  const el = projectCards[position];
 
-mobilePopup.forEach((el) => {
-  let languages = '';
-  el.languages.forEach((lang) => {
-    languages += `<li class="buttons font-f">${lang}</li>`;
+  const pop = document.querySelector('.bg-popup');
+  pop.style.display = 'block';
+
+  const firstCard = document.querySelector('.img-block');
+  firstCard.src = `${el.image}`;
+
+  const seeLive = document.querySelector('.see-live');
+  seeLive.href = `${el.links[0]}`;
+
+  const seeSource = document.querySelector('.see-source');
+  seeSource.href = `${el.links[1]}`;
+
+  document.querySelector('.card-heading-popup').innerText = el.name;
+
+  const btnContiner = document.getElementById('foo');
+  btnContiner.innerHTML = '';
+
+  const paragraph = document.querySelector('.popup-text');
+  paragraph.textContent = `${el.description}`;
+
+  el.technologies.forEach((btn) => {
+    const liBtn = document.createElement('li');
+    liBtn.className = 'buttons font-f';
+    liBtn.innerText = btn;
+    btnContiner.appendChild(liBtn);
   });
 
-  popupData.innerHTML = `
-<li class=" popup">
-          <div class="bg-popup">
-            <div class="popup-data">
-              <i class="fa fa-times popup-close-btn" aria-hidden="true"></i>
-              <img class="img-block" src="${mobilePopup[0].image}" alt="#">
+  document.querySelector('.popup-text').innerText = el.description;
 
-              <div class="bottom-card-popup">
-                <h3 class="card-heading-popup font-f">${mobilePopup[0].title}</h3>
-                <ul class="btns">
-                  ${languages}
-                </ul>
-                <p class="popup-text font-f">${mobilePopup[0].text}</p>
-                <div class="popup-btn-container">
-                  <a href="#" class="popup-btn font-f">${mobilePopup[0].links[0]} <span><i class="fa fa-external-link"></i></span></a>
-                  <a href="#" class="popup-btn font-f">${mobilePopup[0].links[1]} <span><i class="fa fa-github"></i></span></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>`;
-});
-
+  const popupCloseButton = document.querySelector('.popup-close-btn');
+  popupCloseButton.addEventListener('click', () => {
+    const pop = document.querySelector('.bg-popup');
+    pop.style.display = 'none';
+  });
+}
 // project card code
 
-projectCards.forEach((el) => {
+projectCards.forEach((el, i) => {
   let languages = '';
   el.technologies.forEach((lang) => {
     languages += `<li class="buttons font-f">${lang}</li>`;
-  });
-  const projectButtons = document.querySelectorAll('.popOpen');
-
-  projectButtons.forEach((el) => {
-    el.addEventListener('click', () => {
-      const pop = document.querySelector('.bg-popup');
-      pop.style.display = 'block';
-      const popupCloseButton = document.querySelector('.popup-close-btn');
-      popupCloseButton.addEventListener('click', () => {
-        const pop = document.querySelector('.bg-popup');
-        pop.style.display = 'none';
-      });
-    });
   });
 
   const template = document.createElement('template');
 
   template.innerHTML = `<li class="container">
-          <div class="bottom-card">
-            <h3 class="card-heading font-f">${el.name}</h3>
-            <ul class="btns">
-              ${languages}
-            </ul>
-            <button type="button" class="green-btn font-f popOpen">${el.button}</button>
-          </div>
-        </li>`;
+      <div class="bottom-card">
+        <h3 class="card-heading font-f">${el.name}</h3>
+        <ul class="btns">
+          ${languages}
+        </ul>
+        <button type="button" class="green-btn font-f popOpen" onclick="fooBar(${i})">${el.button}</button>
+      </div>
+    </li>`;
 
   grab('cards').appendChild(template.content.firstChild);
 });
